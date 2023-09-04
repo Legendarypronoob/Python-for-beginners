a83c091e2b783c08d2c2e04abb5a534a6c3bb27e
Legendarypronoob <65542841+Legendarypronoob@users.noreply.github.com>
2023-09-04 21:53:50 +0600

    commit

--- python for beginners.py
+++ python for beginners.py
@@ -3,6 +3,7 @@ import time
 import random
 import os
 import shutil
+from abc import ABC, abstractmethod
 
 # # output and data type
 print('single quotation')
@@ -653,42 +654,575 @@ def methodchaining():
 # super function check government class
 
 # abstract classes
+# abstract class = a class which contains one or more abstract methods.
+# abstract method = a method that has a declaration but does not have an implementation.
+
+# prevents a user from creating an object of that class
+# + compels a user to override abstract methods in a child class
+
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def go(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+class Car(Vehicle):
+
+    def go(self):
+        print("You drive the car")
+
+    def stop(self):
+        print("This car is stopped")
+
+
+class Motorcycle(Vehicle):
+
+    def go(self):
+        print("You ride the motorcycle")
+
+    def stop(self):
+        print("This motorcycle is stopped")
+
+
+# vehicle = Vehicle()
+car = Car()
+motorcycle = Motorcycle()
+
+# vehicle.go()
+car.go()
+motorcycle.go()
+
+# vehicle.stop()
+car.stop()
+motorcycle.stop()
+
 
 # objects as arguments
 
+class Car:
+    color = None
+
+
+class Motorcycle:
+    color = None
+
+
+def change_color(vehicle, color):
+    vehicle.color = color
+
+
+car_1 = Car()
+car_2 = Car()
+car_3 = Car()
+
+bike_1 = Motorcycle()
+
+change_color(car_1, "red")
+change_color(car_2, "white")
+change_color(car_3, "blue")
+change_color(bike_1, "black")
+
+print(car_1.color)
+print(car_2.color)
+print(car_3.color)
+print(bike_1.color)
+
+
 # duck typing
+
+# duck typing = concept where the class of an object is less important than the methods/attributes
+#               class type is not checked if minimum methods/attributes are present
+#               “If it walks like a duck, and it quacks like a duck, then it must be a duck.”
+
+class Duck:
+
+    def walk(self):
+        print("This duck is walking")
+
+    def talk(self):
+        print("This duck is qwuacking")
+
+
+class Chicken:
+
+    def walk(self):
+        print("This chicken is walking")
+
+    def talk(self):
+        print("This chicken is clucking")
+
+
+class Person():
+
+    def catch(self, duck):
+        duck.walk()
+        duck.talk()
+        print("You caught the critter!")
+
+
+duck = Duck()
+chicken = Chicken()
+person = Person()
+
+person.catch(chicken)
 
 # walrus operator
 
+# walrus operator :=
+
+# new to Python 3.8
+# assignment expression aka walrus operator
+# assigns values to variables as part of a larger expression
+
+# happy = True
+# print(happy)
+
+# print(happy := True)
+
+# foods = list()
+# while True:
+#   food = input("What food do you like?: ")
+#       if food == "quit":
+#           break
+#   foods.append(food)
+
+foods = list()
+while (food := input("What food do you like?: ")) != "quit":
+    foods.append(food)
+
+
 # functions to variables
+
+def hello():
+    print("Hello")
+
+
+hi = hello
+hi()
+
+
+# say = print
+# say("Whoa! I can't believe this works! :O")
+
 
 # higher order functions
 
-# lamda
+#  Higher Order Function =  a function that either:
+#                           1. accepts a function as an argument
+#                               or
+#                           2. returns a function
+#                           (In python, functions are also treated as objects)
 
+# ---- 1. accepts a function as an argument ----
+def loud(text):
+    return text.upper()
+
+
+def quiet(text):
+    return text.lower()
+
+
+def hello(func):
+    text = func("Hello")
+    print(text)
+
+
+hello(loud)
+hello(quiet)
+
+# ----------- 2. returns a function ------------
+# def divisor(x):
+# def dividend(y):
+# return y / x
+# return dividend
+
+
+# divide = divisor(2)
+# print(divide(10))
+
+# lamda
+# lambda function = function written in 1 line using lambda keyword
+#                   accepts any number of arguments, but only has one expression.
+#                   (think of it as a shortcut)
+#                   (useful if needed for a short period of time, throw-away)
+#
+# lambda parameters:expression
+
+double = lambda g: g * 2
+print(double(1))
+
+multiply = lambda g, h: g * h
+print(multiply(1, 2))
+
+add = lambda g, h, j: g + h + j
+print(add(1, 2, 3))
+
+full_name = lambda first_name, last_name: first_name + " " + last_name
+print(full_name("Bro", "Code"))
 # sort
+
+# sort() method   = used with lists
+# sort() function = used with iterables
+
+students = (("Squidward", "F", 60),
+            ("Sandy", "A", 33),
+            ("Patrick", "D", 36),
+            ("Spongebob", "B", 20),
+            ("Mr.Krabs", "C", 78))
+
+grade = lambda grades: grades[1]
+# students.sort(key=age)                     # sorts current list
+sorted_students = sorted(students, key=grade)  # sorts and creates a new list
+
+for i in sorted_students:
+    print(i)
 
 # map
 
+# map() =   applies a function to each item in an iterable (list, tuple, etc.)
+#
+# map(function,iterable)
+
+store = [("shirt", 20.00),
+         ("pants", 25.00),
+         ("jacket", 50.00),
+         ("socks", 10.00)]
+
+to_euros = lambda data: (data[0], data[1] * 0.82)
+# to_dollars = lambda data: (data[0],data[1]/0.82)
+
+store_euros = list(map(to_euros, store))
+
+for i in store_euros:
+    print(i)
+
 # filter
+
+
+# filter() =    creates a collection of elements from an iterable,
+#               for which a function returns true
+#
+#               filter(function, iterable)
+
+
+# filter() =    creates a collection of elements from an iterable,
+#               for which a function returns true
+#
+#               filter(function, iterable)
+
+friends = [("Rachel", 19),
+           ("Monica", 18),
+           ("Phoebe", 17),
+           ("Joey", 16),
+           ("Chandler", 21),
+           ("Ross", 20)]
+
+age = lambda data: data[1] >= 18
+
+drinking_buddies = list(filter(age, friends))
+
+for i in drinking_buddies:
+    print(i)
 
 # reduce()
 
+# reduce() = apply a function to an iterable and reduce it to a single cumulative value.
+#            performs function on first two elements and repeats process until 1 value remains
+#
+# reduce(function, iterable)
+
+import functools
+
+letters = ["H", "E", "L", "L", "O"]
+word = functools.reduce(lambda x, y,: x + y, letters)
+print(word)
+
+# factorial = [5,4,3,2,1]
+# result = functools.reduce(lambda x, y,:x * y,factorial)
+# print(result)
+
 # list comprehension
+
+# list comprehension =  a way to create a new list with less syntax
+#                       can mimic certain lambda functions, easier to read
+#                       list = [expression for item in iterable]
+#                       list = [expression for item in iterable if conditional]
+#                       list = [expression if/else for item in iterable]
+# --------------------------------------------------------------
+squares = []  # create an empty list
+for i in range(1, 11):  # create a for loop
+    squares.append(i * i)  # define what each loop iteration should do
+print(squares)
+
+# create a list AND defines what each loop iteration should do
+squares = [i * i for i in range(1, 11)]
+print(squares)
+
+# --------------------------------------------------------------
+students = [100, 90, 80, 70, 60, 50, 40, 30, 0]
+
+passed_students = list(filter(lambda x: x >= 60, students))
+passed_students = [i for i in students if i >= 60]
+# passed_students = [i if i >= 60 else "FAILED" for i in students]
+
+print(passed_students)
 
 # dictionary comprehension
 
+# dictionary comprehension = create dictionaries using an expression
+#                            can replace for loops and certain lambda functions
+#
+# dictionary = {key: expression for (key,value) in iterable}
+# dictionary = {key: expression for (key,value) in iterable if conditional}
+# dictionary = {key: (if/else) for (key,value) in iterable}
+# dictionary = {key: function(value) for (key,value) in iterable}
+# -------------------------------------------------------------------------
+cities_in_F = {'New York': 32, 'Boston': 75, 'Los Angeles': 100, 'Chicago': 50}
+cities_in_C = {key: round((value - 32) * (5 / 9)) for (key, value) in cities_in_F.items()}
+print(cities_in_C)
+
+# -------------------------------------------------------------------------
+# weather = {'New York': "snowing", 'Boston': "sunny", 'Los Angeles': "sunny", 'Chicago': "cloudy"}
+# sunny_weather = {key: value for (key,value) in weather.items() if value == "sunny"}
+# print(sunny_weather)
+
+# -------------------------------------------------------------------------
+# cities = {'New York': 32, 'Boston': 75, 'Los Angeles': 100, 'Chicago': 50}
+# desc_cities = {key: ("WARM" if value >= 40 else "COLD") for (key,value) in cities.items()}
+# print(desc_cities)
+
+# -------------------------------------------------------------------------
+# def check_temp(value):
+# if value >= 70:
+# return "HOT"
+# elif 69 >= value >= 40:
+# return "WARM"
+# else:
+# return "COLD"
+
+
+# cities = {'New York': 32, 'Boston': 75, 'Los Angeles': 100, 'Chicago': 50}
+# desc_cities = {key: check_temp(value) for (key,value) in cities.items()}
+# print(desc_cities)
+
 # zip function
+
+# zip(*iterables) =  aggregate elements from two or more iterables (list, tuples, sets, etc.)
+#                    creates a zip object with paired elements stored in tuples for each element
+
+usernames = ["Dude", "Bro", "Mister"]
+passwords = ("p@ssword", "abc123", "guest")
+login_dates = ["1/1/2021", "1/2/2021", "1/3/2021"]
+
+# --------------------------------------
+users = list(zip(usernames, passwords))
+
+for i in users:
+    print(i)
+
+# --------------------------------------
+users = dict(zip(usernames, passwords))
+
+for key, value in users.items():
+    print(key + " : " + value)
+
+# --------------------------------------
+users = zip(usernames, passwords, login_dates)
+
+for i in users:
+    print(i)
+
 
 # if __name__=main
 
+# ***********************************
+# if _name_ == '__main__'
+# ***********************************
+
+# y tho?
+# 1. Module can be run as a standalone program
+#    or
+# 2. Module can be imported and used by other modules
+
+#  Python interpreter sets "special variables", one of which is _name_
+#  Python will assign the _name_ variable a value of '__main__' if it's
+#  the initial module being run
+
+def main():
+    print("Hello!")
+
+
+if __name__ == '__main__':
+    main()
+
+# ***********************************
+
 # time module
+
+# ***************************************************************************
+print(time.ctime(0))  # convert a time expressed in seconds since epoch to a readable string
+#                                        epoch = when your computer thinks time began (reference point)
+print(time.time())  # return current seconds since epoch
+print(time.ctime(time.time()))  # will get current time
+
+# ***************************************************************************
+# time.strftime(format, time_object) = formats a time_object to a string
+# time_object = time.localtime() # local time
+# time_object = time.gmtime()  # UTC time
+# local_time = time.strftime("%B %d %Y %H:%M:%S", time_object)
+# print(local_time)
+
+# ***************************************************************************
+# time.strptime(string_string, format) = parses a string representing time/date and returns a struct_time object
+# time_string = "20 April, 2020"
+# time_object = time.strptime(time_string,"%d %B, %Y")
+# print(time_object)
+
+# ***************************************************************************
+# time.asctime(time_tuple) = accepts a time_object or a tuple up to 9 elements and returns a string
+# (year, month, day, hours, minutes, secs, #day of the week, #day of the year, dst)
+# time_tuple = (2020, 4, 20, 4, 20, 0, 0, 0, 0)
+# time_string = time.asctime(time_tuple)
+# print(time_string)
+
+# ***************************************************************************
+# time.asctime(time_tuple) = accepts a time_object or a tuple up to 9 elements and return seconds since epoch
+# (year, month, day, hours, minutes, secs, #day of the week, #day of the year, dst)
+# time_tuple = (2020, 4, 20, 4, 20, 0, 0, 0, 0)
+# time_string = time.mktime(time_tuple)
+# print(time_string)
 
 # thread
 
+# ******************************************************
+# Python threading tutorial
+# ******************************************************
+# thread =  a flow of execution. Like a separate order of instructions.
+#                  However each thread takes a turn running to achieve concurrency
+#                  GIL = (global interpreter lock),
+#                  allows only one thread to hold the control of the Python interpreter at any one time
+
+# cpu bound = program/task spends most of it's time waiting for internal events (CPU intensive)
+#             use multiprocessing
+
+# io bound = program/task spends most of it's time waiting for external events (user input, web scraping)
+#            use multithreading
+
+import threading
+import time
+
+
+def eat_breakfast():
+    time.sleep(3)
+    print("You eat breakfast")
+
+
+def drink_coffee():
+    time.sleep(4)
+    print("You drank coffee")
+
+
+def study():
+    time.sleep(5)
+    print("You finish studying")
+
+
+x = threading.Thread(target=eat_breakfast, args=())
+x.start()
+
+y = threading.Thread(target=drink_coffee, args=())
+y.start()
+
+z = threading.Thread(target=study, args=())
+z.start()
+
+x.join()
+y.join()
+z.join()
+
+print(threading.active_count())
+print(threading.enumerate())
+print(time.perf_counter())
+
 # demon thread
+
+# Python daemon threads
+# ************************************************************
+
+# daemon thread = a thread that runs in the background, not important for program to run
+#                 your program will not wait for daemon threads to complete before exiting
+#                 non-daemon threads cannot normally be killed, stay alive until task is complete
+#
+#                 ex. background tasks, garbage collection, waiting for input, long running processes
+
+import threading
+import time
+
+
+def timer():
+    print()
+    count = 0
+    while True:
+        time.sleep(1)
+        count += 1
+        print("logged in for: ", count, "seconds")
+
+
+x = threading.Thread(target=timer, daemon=True)
+x.start()
+
+# x.setDaemon(True)
+# print(x.isDaemon())
+
+answer = input("Do you wish to exit?")
+
+# ************************************************************
 
 # multiprocessing
 
-if __name__ == "__main__":
-    methodchaining()
+# *********************************
+# Python multiprocessing
+# *********************************
+# multiprocessing = running tasks in parallel on different cpu cores, bypasses GIL used for threading
+#                   multiprocessing = better for cpu bound tasks (heavy cpu usage)
+#                   multithreading = better for io bound tasks (waiting around)
+
+from multiprocessing import Process, cpu_count
+import time
+
+
+def counter(num):
+    count = 0
+    while count < num:
+        count += 1
+
+
+def main():
+
+    print("cpu count:", cpu_count())
+
+    a = Process(target=counter, args=(500000000,))
+    b = Process(target=counter, args=(500000000,))
+
+    a.start()
+    b.start()
+
+    print("processing...")
+
+    a.join()
+    b.join()
+
+    print("Done!")
+    print("finished in:", time.perf_counter(), "seconds")
+
+
+#if _name_ == '__main__':
+ #   main()
+
+# *********************************
